5ddf227fe4b36d20ec9c08f1b71f0623499c7e94
viktorija <viktorija.gaizutyte@gmail.com>
2014-08-19 21:58:48 +0200

    some coding (not functional..)

---
+++ game/Assets/scripts/playerControls.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(playerPhysics))]
+public class playerControls : MonoBehaviour {
+
+	// player controls
+
+	public float speed =8;
+	public float acceleration=12;
+
+	private float currentSpeed;
+	private float maxSpeed;
+	private Vector2 distToMove;
+
+	private playerPhsyics playerPhysics; 
+
+
+	void Start () {
+		playerPhsyics = GetComponent<playerPhysics>(); 
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		maxSpeed = Input.GetAxisRaw ("Horizontal") * speed;
+		currentSpeed = IncrementTowards (currentSpeed, maxSpeed, acceleration);
+
+		distToMove = new Vector2 (currentSpeed, 0);
+		playerPhysics.Move (distToMove * Time.deltaTime);
+	}
+
+	// increase speed through acceleration/ using increment method
+
+	private float IncrementTowards (float n, float target, float a) {
+		if (n== target) {
+			return n; 
+		}
+		else { 
+			float dir = Mathf.Sign (target - n);  
+			n += speed * Time.deltaTime * dir;
+			return (dir == Mathf.Sign(target-n))? n: target; 
+
+		}
+	}
+
+
+}
---
+++ game/Assets/scripts/playerPhysics.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerPhysics : MonoBehaviour {
+
+	public void Move(Vector2 distMoved) {
+
+		transform.Translate (distMoved);
+}
